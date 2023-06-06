7a4cf63c6b127f7a4bd13460f1c13c509ec483d7
mindegyt <tbotond@protonmail.com>
2023-06-06 09:19:07 +0200

    API

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 function kiir() {
+  fetch("https://api.coingecko.com/api/v3/exchange_rates", {
+    headers: {
+      accept: "application/json",
+    },
+    method: "GET",
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      const rates = data.rates;
+      document.getElementById("eur").textContent = rates.eur.value + " EUR";
+      document.getElementById("usd").textContent = rates.usd.value + " USD";
+      document.getElementById("gbp").textContent = rates.gbp.value + " GBP";
+      document.getElementById("chf").textContent = rates.chf.value + " CHF";
+      document.getElementById("pln").textContent = rates.pln.value + " PLN";
+      document.getElementById("huf").textContent = rates.huf.value + " HUF";
 
-fetch("https://api.coingecko.com/api/v3/exchange_rates", {
-  "headers": {
-    "accept": "application/json"
-  },
-  "method": "GET",
-}).then(adat => adat.json())
-.then(res => {
-    document.getElementById("teny").innerHTML = res["fact"];
-})
+    });
 }
+kiir();
